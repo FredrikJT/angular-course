1f1bcfb0651cc3f86e1937009f37f1ef8dbe2309
Fredrik Johansson Torneus <fj3279@tobii.com>
2020-03-02 18:49:43 +0100

    sammanfattning

--- sammanfattning.ts
+++ sammanfattning.ts
@@ -8,3 +8,23 @@
     //[(ngModel)]="data"
 
 //EVENT BINDING: B
+
+
+//DIRECTIVES: Directives are instructions in the DOM. E.g. ngIf
+// <p *ngIf="false">This wont be shown</p>
+//* says that its a sturctural directive, i.e. changing the DOM
+
+//<p *ngIf="false; else noServer">Call noServer template</p>
+//<ng-template #noServer>
+//  <p>No server created</p>
+//</ng-template>
+
+//Attribute directives dont adsd or remove elements, they only change the element they
+//are placed on.
+//>p [ngStyle]="{backgroundColor: getColor()}">
+
+
+
+getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+}
