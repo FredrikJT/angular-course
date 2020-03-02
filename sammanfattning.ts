8d628fee508a2aba92d598625414e38fec3fb977
Fredrik Johansson Torneus <fj3279@tobii.com>
2020-03-02 09:22:39 +0100

    Initial commit. Beginning of sammanfattning

---
+++ sammanfattning.ts
@@ -0,0 +1,10 @@
+//DATABINDING: Communicate between business logic (TS) and the template (HTML). 24
+//BS => Template
+    //String interpolation {{ data }} (output the value of a variable)
+    //Property binding [property]="data" (bind some kind of data, e.g. a variable to some property of the DOM)
+//Template => BS (React to user events)
+    //Event binding (event)="expression"
+//Two-way-binding
+    //[(ngModel)]="data"
+
+//EVENT BINDING: B
