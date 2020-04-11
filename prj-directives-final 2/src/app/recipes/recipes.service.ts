4c423d8ce9c1972e8358e021f0532c327ae27e74
Fredrik Johansson Torneus <fredrik.j.torneus@gmail.com>
2020-04-11 17:38:12 +0200

    My final porject routing code

--- prj-directives-final 2/src/app/recipes/recipes.service.ts
+++ prj-directives-final 2/src/app/recipes/recipes.service.ts
@@ -1,5 +1,44 @@
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list-service';
 
 export class RecipesService {
   recipe: Recipe;
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Tasty schnitzel',
+      'Super tastu',
+      'https://en.wiktionary.org/wiki/food#/media/File:Good_Food_Display_-_NCI_Visuals_Online.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('fries', 20)
+      ]
+    ),
+    new Recipe(
+      'Da food',
+      'A lot',
+      'https://en.wiktionary.org/wiki/food#/media/File:Good_Food_Display_-_NCI_Visuals_Online.jpg',
+      [
+        new Ingredient('veggies', 5),
+        new Ingredient('Food', 1)
+      ]
+    )
+  ];
+
+  constructor(private slService: ShoppingListService) {
+  }
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+
 }
---
+++ prj-directives-final 2/src/app/shopping-list/shopping-list-service.ts
@@ -0,0 +1,8 @@
+import { Ingredient } from "../shared/ingredient.model";
+
+
+export class ShoppingListService {
+  addIngredients(ingredients: Ingredient[]) {
+
+  }
+}
