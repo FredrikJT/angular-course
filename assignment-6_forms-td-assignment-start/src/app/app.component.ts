abc3a0cfc4083c0658e0dc6801bb85ab7791bb49
Fredrik Johansson Torneus <fredrik.j.torneus@gmail.com>
2020-04-21 09:40:47 +0200

    print form object

--- assignment-6_forms-td-assignment-start/src/app/app.component.ts
+++ assignment-6_forms-td-assignment-start/src/app/app.component.ts
@@ -6,8 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  onSubmit() {
-    console.log('submitted');
+  onSubmit(form: HTMLFormElement) {
+    console.log(form);
   }
 
 }
