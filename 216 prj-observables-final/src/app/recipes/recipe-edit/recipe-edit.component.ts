bc752e592cacc6b8762e6623d7fe2b06f20d4b8a
Fredrik Johansson Torneus <fredrik.j.torneus@gmail.com>
2020-05-01 09:55:56 +0200

    Creating HTML form for new recipe. Lecture 225

--- 216 prj-observables-final/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ 216 prj-observables-final/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-edit',
